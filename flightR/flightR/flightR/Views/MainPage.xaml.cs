38b688dda5f445a8b26d9b254d34e71a85d12047
enesozturk <enesozturk.dev@gmail.com>
2017-08-06 19:21:37 +0300

    Initial commit

---
+++ flightR/flightR/flightR/Views/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using flightR.Views.Tabs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace flightR.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Children.Add(new NewRecord());
+            Children.Add(new Profile());
+            Children.Add(new Settings());
+        }
+    }
+}
---
+++ flightR/flightR/flightR/Views/Tabs/NewRecord.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace flightR.Views.Tabs
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewRecord : ContentPage
+    {
+        public NewRecord()
+        {
+            InitializeComponent();
+        }
+    }
+}
